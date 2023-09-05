1817093845942a855342af82443251a2eff9f725
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-05 13:12:27 -0700

    added cors

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
-
+const cors = require("cors");
 const ProductRouter = require("./routes/product");
 
 app.use(express.json());
+app.use(cors());
 app.use(
 	express.urlencoded({
 		extended: true
