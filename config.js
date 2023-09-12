c76ac638e7129afb287cf42fb8b4766f269eb293
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-11 17:04:00 -0700

    post testing

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const config = {
 	db: {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USERNAME,
-		port: process.env.DB_PORT,
 		password: process.env.DB_PASSWORD,
 		database: process.env.DB_DATABASE,
 		connectTimeout: 60000
 	},
 	listPerPage: 10
 };
-module.exports = config;
+
+module.exports = { config };
--- services/product.js
+++ services/product.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const db = require("./db");
 const helper = require("../helper");
 const config = require("../config");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+	cloud_name: process.env.cloud_name,
+	api_key: process.env.api_key,
+	api_secret: process.env.api_secret
+});
+
+console.log(cloudinary.config().cloud_name);
 
 async function getMultiple(page = 1) {
 	const offset = helper.getOffset(page, config.listPerPage);
@@ -17,12 +27,20 @@ async function getMultiple(page = 1) {
 }
 
 async function create(product) {
-	const result = await db.query(
-		`INSERT INTO Product 
-		(name, description, brand, image, SKU, category, size, price)
-		VALUES
-		(${product.name}, ${product.description}, ${product.brand}, ${product.image}, ${product.sku}, ${product.category}, ${product.size}. ${product.price})`
-	);
+	try {
+		const cloud_res = await cloudinary.uploader.upload(product.image, {
+			resource_type: "image"
+		});
+		const image_url = cloud_res.url;
+		const result = await db.query(
+			`INSERT INTO Product
+			(name, description, brand, image, SKU, category, size, price)
+			VALUES
+			(${product.name}, ${product.description}, ${product.brand}, ${image_url}, ${product.sku}, ${product.category}, ${product.size}. ${product.price})`
+		);
+	} catch {
+		console.log("error", JSON.stringify(err, null, 2));
+	}
 
 	let message = "Error in creating new product";
 
@@ -30,7 +48,7 @@ async function create(product) {
 		message = "New product created successfully";
 	}
 
-	return { message };
+	return { result };
 }
 
 module.exports = {
