ce66d6971226c3a6357cdc3b447f1119dae72c50
dnguyen130 <dnguyen130@my.bcit.ca>
2023-09-11 17:27:21 -0700

    revert cloudinary

--- services/product.js
+++ services/product.js
@@ -1,16 +1,6 @@
-require("dotenv").config();
 const db = require("./db");
 const helper = require("../helper");
 const config = require("../config");
-const cloudinary = require("cloudinary").v2;
-
-cloudinary.config({
-	cloud_name: process.env.cloud_name,
-	api_key: process.env.api_key,
-	api_secret: process.env.api_secret
-});
-
-console.log(cloudinary.config().cloud_name);
 
 async function getMultiple(page = 1) {
 	const offset = helper.getOffset(page, config.listPerPage);
@@ -28,15 +18,11 @@ async function getMultiple(page = 1) {
 
 async function create(product) {
 	try {
-		const cloud_res = await cloudinary.uploader.upload(product.image, {
-			resource_type: "image"
-		});
-		const image_url = cloud_res.url;
 		const result = await db.query(
 			`INSERT INTO Product
 			(name, description, brand, image, SKU, category, size, price)
 			VALUES
-			(${product.name}, ${product.description}, ${product.brand}, ${image_url}, ${product.sku}, ${product.category}, ${product.size}. ${product.price})`
+			(${product.name}, ${product.description}, ${product.brand}, ${product.image}, ${product.sku}, ${product.category}, ${product.size}. ${product.price})`
 		);
 	} catch (err) {
 		console.log("error: ", err);
